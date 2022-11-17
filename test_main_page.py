b8df515321e6dd4ec3ba1bd3fb71d5082cc5cd3e
admin <beta2adrenoreceptor@ya.ru>
2022-11-18 02:17:06 +0500

    added core files

---
+++ test_main_page.py
@@ -0,0 +1,14 @@
+import selenium.common.exceptions
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
