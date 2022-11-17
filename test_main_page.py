450601fc42fae65ce6477a6e3a504a7a6b5a85d1
admin <beta2adrenoreceptor@ya.ru>
2022-11-18 03:38:58 +0500

    added new class BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,14 @@ import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import math
 
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+    time.sleep(5)
